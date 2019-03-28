18d4dfa583c078ed47ecb7d6ea6a430b1a8754b7
Feriixu <feriixu@users.noreply.github.com>
2019-03-28 12:57:24 +0100

    Initial commit

---
+++ CharTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CharTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = (char)0b0;
+            while (true)
+            {
+                Console.WriteLine(ch.ToString());
+                Thread.Sleep(100);
+                ch++;
+            }
+
+        }
+    }
+}
